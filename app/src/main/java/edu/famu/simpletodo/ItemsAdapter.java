de1cb79bdab97b81ed03ac42e2931f50dbc2d7fc
Nana A. Marfo <nmarfo@cis.famu.edu>
2020-01-29 21:58:06 -0500

    Added delete and persistence

--- app/src/main/java/edu/famu/simpletodo/ItemsAdapter.java
+++ app/src/main/java/edu/famu/simpletodo/ItemsAdapter.java
@@ -13,10 +13,17 @@ import androidx.recyclerview.widget.RecyclerView;
 //Responsible for dispalying data from the model into a row in the reycler view
 public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ViewHolder> {
 
+    public interface  OnLongClickListener{
+        void onItemLongClicked(int position);
+    }
     List<String> items;
+    OnLongClickListener longClickListener;
 
-    public ItemsAdapter(List<String> items) {
+    public ItemsAdapter(List<String> items, OnLongClickListener longClickListener) {
+
         this.items = items;
+        this.longClickListener = longClickListener;
+
     }
 
     @NonNull
@@ -56,6 +63,15 @@ public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ViewHolder> 
         //Update the view inside of the view holder with the data
         public void bind(String item) {
             tvItem.setText(item);
+            tvItem.setOnLongClickListener(new View.OnLongClickListener(){
+                @Override
+                public  boolean onLongClick(View v) {
+                    //Notify the listener which postion was long pressed
+                    longClickListener.onItemLongClicked(getAdapterPosition());
+                    return true;
+
+                }
+            });
         }
     }
 }
